c5f83a7cc65eed12a39fda8af36647ee19704fb5
Nima <nimarahimijahandide@gmail.com>
2025-07-17 21:14:03 +0330

    fix header

--- src/components/PortActivityTable.vue
+++ src/components/PortActivityTable.vue
@@ -35,7 +35,7 @@
               <div class="absolute right-0 top-[6px] bottom-[6px] w-px bg-gray-300"></div>
             </th>
             <th class="relative px-3 py-2 text-left">
-              <span>To Date &amp; Time</span>
+              <span>To Date Time</span>
               <div class="absolute right-0 top-[6px] bottom-[6px] w-px bg-gray-300"></div>
             </th>
             <th class="relative px-3 py-2 text-left">
